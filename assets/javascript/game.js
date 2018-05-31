c7cd338d4309e49c4480760b8ce3b3934bbef7cc
tylenick <tylerlenick@gmail.com>
2018-05-31 13:09:28 -0400

    JS working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,55 +2,95 @@
 
 var numWins = 0;
 var numGuess = 12;
+var gameOn = false;
+var randomWord = "";
 
 // Declare arrays
 
-var lettersGuessed = [];
 var words = ['cat', 'dog', 'fish'];
+var correctLetter = [];
+var wrongLetter = [];
+var selectedWord = [];
 
-//Choose word from array randomly
 
-var randomNum = Math.floor(Math.random() * words.length);
-var randomWord = words[randomNum];
-var underScore = [];
 
-var selectedWord = randomWord.split("");
-
-console.log(selectedWord);
-
-//Function used to determine length of word
-
-function wordLength() {
-    
+//Function used when user clicks start the game button to reset all needed variables.
+function newGame() {
+ 
+    gameOn = true;
+    numGuess = 12;
+    correctLetter = [];
+    wrongLetter = [];
+    randomWord = words[Math.floor(Math.random() * words.length)];
+    selectedWord = randomWord.split("");
+    document.getElementById("word-area").innerHTML = correctLetter;
     for (var i = 0; i < randomWord.length; i++) {
-        underScore.push("_");
+        correctLetter.push("_");
     }
-    return underScore;
+    console.log("New game");
+    console.log(randomWord);
+    
+}
+
+//Function used to determine if all letters in the selected word have been changed to "" meaning the user wins.
+function testWin(win) {
+    return win === "";
 }
 
 
 //Event handler on key press.
-
 document.onkeyup = function(event) {
 
-    var letter = event.key.toLowerCase();
-    
-    // console.log(randomWord);
-    // console.log(letter.indexOf(randomWord));
-    //if letter = indexof word selected, show that letter as guessed correctly
+    //If the game is running, run through the event.
+    if (gameOn) {
+        
+        var letter = event.key.toLowerCase();
+        console.log(letter.charCodeAt(0))
+        if (letter.charCodeAt(0) >= 97 && letter.charCodeAt(0) <= 122) {
 
-    if (letter.indexOf(selectedWord) > -1) {
-        console.log(true);
-    } else {
-        console.log(false);
+            //if letter = indexof word selected, show that letter as guessed correctly
+            if (selectedWord.indexOf(letter) > -1) {
+                
+                correctLetter[selectedWord.indexOf(letter)] = letter;
+                selectedWord[selectedWord.indexOf(letter)] = "";
+                
+                
+            } else if (wrongLetter.indexOf(letter) === -1) {
+                //add to the wrongLetter array and decrement numGuess
+                wrongLetter.push(letter);
+                numGuess--;
+                console.log(numGuess);
+            }
+
+
+            //Check if the selectedWord array has been emptied, meaning the user has guessed everything correctly.
+            if (selectedWord.every(testWin)) {
+                numWins++;
+                console.log("you win!!");
+                console.log(numWins)
+                newGame();
+                
+            
+            //If numGuess = 0, reset game
+            } else if (numGuess === 0) {
+                newGame();
+                console.log("You lose!!!!!!");
+            }
+                
+        } else {
+            console.log("Enter valid key");
+        }
+
     }
+    console.log(correctLetter);
+    console.log(wrongLetter);
 
-    //else subtract 1 from numGuess, add letter to letters guessed array.
 }
+      
+    
 
 
 
 
-//If numGuess = 0, reset game
 
-//If all letters guessed correctly reset game, + 1 numWins. 
+
